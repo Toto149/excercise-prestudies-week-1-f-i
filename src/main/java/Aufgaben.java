c93a4e599f7cd8594a1310173d165a468f343b20
Thorsten Thomann <thorsten.thomann@googlemail.com>
2024-04-18 22:49:17 +0200

    refactoring

--- src/main/java/Aufgaben.java
+++ src/main/java/Aufgaben.java
@@ -4,17 +4,86 @@ import java.util.Scanner;
 
 public class Aufgaben {
     public static void main(String[] args){
-        // Aufgabe 1.1 Scanner
+
         // Methodendeklarationen zu den einzelnen Methoden in den Aufgaben unten in dieser Klasse.
+        // Wenn Methoden ausgeblendet werden sollen, können diese einfach auskommentiert werden.
+        // Mit Strg + Linksclick auf dem Methodennamen kann man zu den Methoden navigieren.
 
-            System.out.println("Aufgabe 1.1 Scanner");
-
+        // Aufgabe 1.1 Scanner
             textInputOutput();
 
         // Aufgabe 1.2: Formatting text output
+            formattingTextOutput();
 
-            System.out.println("\n\nAufgabe 1.2: Formatting text output\n");
-            System.out.println( """
+        // Aufgabe 1.3 Geometry
+            geometry();
+
+
+        // Datatypes & Variables
+        // Aufgabe 2.1 Data Types
+            dataTypes();
+
+        // Aufgabe 2.2 Formatting Text(Bonus)
+            System.out.println("\n\nAufgabe 2.2 Formatting Text(Bonus)\n");
+
+            String input = "Everybody _said_, it can't be done. Then came one who did not #KNOW# that and just made it.";
+            String output = formattingText(input);
+            System.out.println(output);
+
+        // Aufgabe 3.1
+            System.out.println("\n\nAufgabe 3.1\n");
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Enter a number of seconds: ");
+            int numberOfSeconds = scanner.nextInt();
+
+            System.out.println("Number of seconds: 244568879 are converted to (in years days hours minutes and seconds):");
+            System.out.println(numberOfSecondsToYearsDaysHoursMinutesSeconds(numberOfSeconds));
+
+        // Aufgabe 3.2
+            cylinder();
+
+        // Aufgabe 3.3 One line of Code
+            oneLineOfCode();
+
+        // Aufgabe 3.4 Metropolis
+            System.out.println("\n\nAufgabe 3.4 Metropolis");
+
+            metropolisChecker("München", true,1200000,1000);
+
+        // Loops
+        // Aufgabe 4.1 Guessing numbers
+            guesstheNumber();
+
+        // Aufgabe 4.2 Triangles
+            triangles();
+
+        //Aufgabe 4.3 Christmas tree
+            christmasTree();
+    }
+
+    // Methoden zu den Aufgaben von oben hier
+
+    // Methode zur Aufgabe 1.1 Text input and output
+
+    public static void textInputOutput(){
+        System.out.println("Aufgabe 1.1 Scanner");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name:");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name +", Welcome to the training!" );
+    }
+
+    // Methode zur Aufgabe 1.2 Ascii Print
+
+    public static void formattingTextOutput(){
+
+        System.out.println("\n\nAufgabe 1.2: Formatting text output\n");
+
+        System.out.println( """
                       _______________                        |*\\_/*|________
                       |  ___________  |     .-.     .-.      ||_/-\\_|______  |
                       | |           | |    .****. .****.     | |           | |
@@ -29,173 +98,16 @@ public class Aufgaben {
                     --------------------                    --------------------
                     
                     """
-                );
+        );
 
-        // Aufgabe 1.3 Geometry
-
-            System.out.println("\n\nAufgabe 1.3 Geometry\n");
-            geometry();
-
-
-        // Datatypes & Variables
-
-        // Aufgabe 2.1 Data Types
-
-            System.out.println("\n\nAufgabe 2.1 Data Types");
-
-            //Initals of my name
-
-                System.out.println("Initials of my name as char since each of them is only one letter which can be stored in a" +
-                        " char as smallest data type to store it in (16 bit)");
-
-                char myFirstNameInitial = 'T';
-                System.out.println("My first name initial:  " + myFirstNameInitial);
-
-                char myMiddleNameInitial = 'J';
-                System.out.println("My middle name initial:  " + myMiddleNameInitial);
-
-                char myLastNameInitial = 'T';
-                System.out.println("My last name initial:  " +myLastNameInitial);
-
-            /* Population of Germany. Int (32 bit) is the smallest that fits the population, as short (16 bit) is too small for it to fit.**/
-
-                System.out.println("\nPopulation of Germany. Int (32 bit) is the smallest that fits the population," +
-                        " as short (16 bit) is to small for it to fit.");
-
-                int populationOfGermany = 83862225;
-                System.out.println("Population of Germany: " + populationOfGermany);
-
-
-            /* Population of Earth. Since 2^31-1 (highest number in int) is roughly 1.6 billion and something, the population of Earth is to big to
-                 fit in the int Datatype. So we have to use the next bigger Datatype to represent integers and that is long (64 bit) **/
-
-                System.out.println(
-                        """
-                           
-                           Population of Earth. Since 2^31-1 (highest number in int) is roughly 1.6 billion and something,
-                           the population of Earth is to big to fit in the int Datatype. So we have to use the next bigger 
-                           Datatype to represent integers and that is long (64 bit).
-                           """);
-
-                long populationOfEarth = 8147701969L;
-                System.out.println("Human population of Earth: " + populationOfEarth);
-
-            /*Smallest Datatype to fit an yes/no answer is boolean (1-bit).**/
-
-                System.out.println("\nSmallest Datatype to fit an yes/no answer is boolean (1 bit).\n");
-
-                boolean isDayTime = true;
-                System.out.println("Is it day time? " + isDayTime);
-
-
-            /* Goal strike quote of Mario gomez will very likely be an number between 0 and 1, so we have to use float (32 bit)
-                since it is the smallest datatype in Java for floating point numbers. **/
-
-                System.out.println("""
-                        
-                        Goal strike quote of Mario gomez will very likely be an number between 0 and 1,
-                        so we have to use float (32 bit) since it is the smallest datatype in Java for floating point numbers.
-                        """);
-
-                float marioGomezGoalStrikeQuoteAtFCB = 0.65217f;
-                System.out.println("Mario Gomez goal strike quote as player for FC Bayern München: " + marioGomezGoalStrikeQuoteAtFCB + " goals per game.");
-
-            /*Length of the Java Programm in weeks. Since it is about 16 weeks long including the pre-studies. It fits in
-                the smallest numerical DataType, which is Byte (8 bit). **/
-
-                System.out.println("""
-                        
-                        Length of the Java Programm in weeks. Since it is about 16 weeks long
-                        including the pre-studies. It fits in the smallest numerical DataType, which is Byte (8 bit).
-                        """);
-
-                byte javaProgramLengthInWeeks = 14;
-                System.out.println("Java Bootcamp Länge in Wochen: " + javaProgramLengthInWeeks);
-
-            /* The mathematical number PI. Doesn't really fit in any datatype since it's information size is infinite.
-                 An good enough approximation fits in float. For higher precision it is recommended to use double or BigDecimal. **/
-
-                System.out.println("""
-                        
-                        The mathematical number PI. Doesn't really fit in any datatype since it's information size is infinite. 
-                        An good enough approximation fits in float (maximal 6-7 digits). For higher precision it is recommended 
-                        to use double (64 bit) or BigDecimal (varies in size, object).
-                        """);
-
-                final float PI = 3.14159f; //Use final for constant, Constants are also written in all caps.
-                System.out.println("Value of pi in float: " + PI);
-
-        // Aufgabe 2.2 Formatting Text(Bonus)
-            System.out.println("\n\nAufgabe 2.2 Formatting Text(Bonus)\n");
-
-            String input = "Everybody _said_, it can't be done. Then came one who did not #KNOW# that and just made it.";
-            String output = formattingText(input);
-            System.out.println(output);
-
-
-
-        // Aufgabe 3.1
-            System.out.println("\n\nAufgabe 3.1\n");
-
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Enter a number of seconds: ");
-            int numberOfSeconds = scanner.nextInt();
-
-            System.out.println("Number of seconds: 244568879 are converted to (in years days hours minutes and seconds):");
-            System.out.println(numberOfSecondsToYearsDaysHoursMinutesSeconds(numberOfSeconds));
-
-        // Aufgabe 3.2
-            System.out.println("\n\nAufgabe 3.2\n");
-
-            cylinder();
-
-        // Aufgabe 3.3 One line of Code
-            System.out.println("\n\nAufgabe 3.3 One line of Code\n");
-
-            double inOneLine =  ((Math.sqrt(3.5 +15.5)*5)/3) - ((15.5 + 10) * (15.5 - 4.1));
-            System.out.println("The result of the calculation in one line is: ");
-            System.out.println(inOneLine);
-
-        // Aufgabe 3.4 Metropolis
-            System.out.println("\n\nAufgabe 3.4 Metropolis");
-
-            metropolisChecker("München", true,1200000,1000);
-
-        // Loops
-        // Aufgabe 4.1 Guessing numbers
-            System.out.println("\n\nAufgabe 4.1 Guessing numbers\n");
-
-            guesstheNumber();
-
-
-        // Aufgabe 4.2 Triangles
-            System.out.println("\n\nAufgabe 4.2 Triangles\n");
-
-            triangles();
-
-        //Aufgabe 4.3 Christmas tree
-            System.out.println("\n\nAufgabe 4.3 Christmas tree\n");
-
-            christmasTree();
-    }
-
-    // Methoden zu den Aufgaben oben hier
-
-    // Methode zur Aufgabe 1.1 Text input and output
-
-    public static void textInputOutput(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Please enter your name:");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name +", Welcome to the training!" );
     }
 
 
     // Methode zur Aufgabe 1.3 Geometry
 
     public static void geometry(){
+        System.out.println("\n\nAufgabe 1.3 Geometry\n");
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Gebe die länge der erste Seite a eines Rechtecks bzw. der Ankathete eines rechtwinkligen Dreiecks an(Kommazahl (e.g. 2,5) erlaubt):");
@@ -215,6 +127,94 @@ public class Aufgaben {
 
         double perimeterRightTriangle =  Math.sqrt((Math.pow(sideA,2) + Math.pow(sideB,2)));
         System.out.println("Fläche des rechtwinkligen Dreiecks: " + perimeterRightTriangle);
+    }
+
+    // Methode zur Aufgabe 2.1
+
+    public static void dataTypes(){
+        System.out.println("\n\nAufgabe 2.1 Data Types");
+
+        //Initals of my name
+
+        System.out.println("Initials of my name as char since each of them is only one letter which can be stored in a" +
+                " char as smallest data type to store it in (16 bit)");
+
+        char myFirstNameInitial = 'T';
+        System.out.println("My first name initial:  " + myFirstNameInitial);
+
+        char myMiddleNameInitial = 'J';
+        System.out.println("My middle name initial:  " + myMiddleNameInitial);
+
+        char myLastNameInitial = 'T';
+        System.out.println("My last name initial:  " +myLastNameInitial);
+
+        /* Population of Germany. Int (32 bit) is the smallest that fits the population, as short (16 bit) is too small for it to fit.**/
+
+        System.out.println("\nPopulation of Germany. Int (32 bit) is the smallest that fits the population," +
+                " as short (16 bit) is to small for it to fit.");
+
+        int populationOfGermany = 83862225;
+        System.out.println("Population of Germany: " + populationOfGermany);
+
+
+            /* Population of Earth. Since 2^31-1 (highest number in int) is roughly 1.6 billion and something, the population of Earth is to big to
+                 fit in the int Datatype. So we have to use the next bigger Datatype to represent integers and that is long (64 bit) **/
+
+        System.out.println(
+                """
+                   
+                   Population of Earth. Since 2^31-1 (highest number in int) is roughly 1.6 billion and something,
+                   the population of Earth is to big to fit in the int Datatype. So we have to use the next bigger 
+                   Datatype to represent integers and that is long (64 bit).
+                   """);
+
+        long populationOfEarth = 8147701969L;
+        System.out.println("Human population of Earth: " + populationOfEarth);
+
+        /*Smallest Datatype to fit an yes/no answer is boolean (1-bit).**/
+
+        System.out.println("\nSmallest Datatype to fit an yes/no answer is boolean (1 bit).\n");
+
+        boolean isDayTime = true;
+        System.out.println("Is it day time? " + isDayTime);
+
+
+            /* Goal strike quote of Mario gomez will very likely be an number between 0 and 1, so we have to use float (32 bit)
+                since it is the smallest datatype in Java for floating point numbers. **/
+
+        System.out.println("""
+                        
+                        Goal strike quote of Mario gomez will very likely be an number between 0 and 1,
+                        so we have to use float (32 bit) since it is the smallest datatype in Java for floating point numbers.
+                        """);
+
+        float marioGomezGoalStrikeQuoteAtFCB = 0.65217f;
+        System.out.println("Mario Gomez goal strike quote as player for FC Bayern München: " + marioGomezGoalStrikeQuoteAtFCB + " goals per game.");
+
+            /*Length of the Java Programm in weeks. Since it is about 16 weeks long including the pre-studies. It fits in
+                the smallest numerical DataType, which is Byte (8 bit). **/
+
+        System.out.println("""
+                        
+                        Length of the Java Programm in weeks. Since it is about 16 weeks long
+                        including the pre-studies. It fits in the smallest numerical DataType, which is Byte (8 bit).
+                        """);
+
+        byte javaProgramLengthInWeeks = 14;
+        System.out.println("Java Bootcamp Länge in Wochen: " + javaProgramLengthInWeeks);
+
+            /* The mathematical number PI. Doesn't really fit in any datatype since it's information size is infinite.
+                 An good enough approximation fits in float. For higher precision it is recommended to use double or BigDecimal. **/
+
+        System.out.println("""
+                        
+                        The mathematical number PI. Doesn't really fit in any datatype since it's information size is infinite. 
+                        An good enough approximation fits in float (maximal 6-7 digits). For higher precision it is recommended 
+                        to use double (64 bit) or BigDecimal (varies in size, object).
+                        """);
+
+        final float PI = 3.14159f; //Final Constants are written in Caps
+        System.out.println("Value of pi in float: " + PI);
     }
 
 
@@ -263,6 +263,7 @@ public class Aufgaben {
 
     // Methode zur Aufgabe 3.2 Cylinder
     public static void cylinder(){
+        System.out.println("\n\nAufgabe 3.2\n");
 
         Scanner scanner = new Scanner(System.in);
 
@@ -293,7 +294,15 @@ public class Aufgaben {
     }
 
 
+    // Methoder zur Aufgabe 3.3
 
+    public static void oneLineOfCode(){
+        System.out.println("\n\nAufgabe 3.3 One line of Code\n");
+
+        double inOneLine =  ((Math.sqrt(3.5 +15.5)*5)/3) - ((15.5 + 10) * (15.5 - 4.1));
+        System.out.println("The result of the calculation in one line is: ");
+        System.out.println(inOneLine);
+    }
     // Methode zur Aufgabe 3.4 Metropolis (Bonus excercise)
 
     public static void metropolisChecker(String nameOfCity, boolean isCapital, int population, double taxPerPerson) {
@@ -343,6 +352,8 @@ public class Aufgaben {
     // Methode zur Aufgabe 4.1 Guessing the numbers
 
     public static void guesstheNumber(){
+        System.out.println("\n\nAufgabe 4.1 Guessing numbers\n");
+
         int randomNumberToGuess = (int) (Math.random() * 99) +1;
         Scanner scanner = new Scanner(System.in);
 
@@ -372,6 +383,8 @@ public class Aufgaben {
     // Methode für Aufgabe 4.2 Triangles
 
     public static void triangles(){
+        System.out.println("\n\nAufgabe 4.2 Triangles\n");
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("How large should the triangle be?");
@@ -397,6 +410,8 @@ public class Aufgaben {
     // Methode für Aufgabe 4.3 Christmas tree (Bonus)
 
     public static void christmasTree(){
+        System.out.println("\n\nAufgabe 4.3 Christmas tree\n");
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("How many lines high should the treetop be?");
